da3a014405264b0b6b3e6a0a8ec4531a51c9717c
Qwetsh <100966583+Qwetsh@users.noreply.github.com>
2023-11-11 18:22:57 +0100

    Add files via upload

---
+++ public/App.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React, { useState } from "react";
+import WorkSiteInProgress from "./Pages/WorkSiteInProgress";
+
+
+
+
+function App() {
+  
+  return (
+   <WorkSiteInProgress></WorkSiteInProgress>
+  );
+}
+
+export default App;
