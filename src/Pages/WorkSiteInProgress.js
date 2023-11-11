2773826f749d64cffead8524e48d2aaa2ec46f77
Qwetsh <100966583+Qwetsh@users.noreply.github.com>
2023-11-11 18:23:28 +0100

    Add files via upload

--- src/Pages/WorkSiteInProgress.js
+++ src/Pages/WorkSiteInProgress.js
@@ -1,35 +1,35 @@
-// import Classes from "./WorkSiteInProgress.module.css";
+import Classes from "./WorkSiteInProgress.module.css";
 import WorkSite from "../Components/WorkSite";
 import WorksiteForm from "../Components/WorksiteForm";
 import NewWorkSiteBtn from "../Components/newWorkSiteBtn";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 function WorkSiteInProgress(props) {
   const [NewWorkSiteBtnisClicked, setIsClicked] = useState(false);
 
   return (
     <>
-      <header className="App-header">
-        <WorkSite changeState={NewWorkSiteBtnisClicked}/>
-        {NewWorkSiteBtnisClicked && (
-          <WorksiteForm
-            onClick={() => {
-              setIsClicked(!NewWorkSiteBtnisClicked);
-            }}
-          />
-        )}
+
+        <div className={Classes.bigContainer}>
+          <WorkSite changeState={NewWorkSiteBtnisClicked} />
+          {NewWorkSiteBtnisClicked && (
+            <WorksiteForm
+              onClick={() => {
+                setIsClicked(!NewWorkSiteBtnisClicked);
+              }}
+            />
+          )}
+        </div>
+
         {!NewWorkSiteBtnisClicked && (
-          <NewWorkSiteBtn
-            onClick={() => {
-              setIsClicked(!NewWorkSiteBtnisClicked);
-            }}
-          />
-        )}
-      </header>
+        <NewWorkSiteBtn
+          onClick={() => {
+            setIsClicked(!NewWorkSiteBtnisClicked);
+          }}
+        />
+      )}
     </>
   );
 }
-
-
 
 export default WorkSiteInProgress;
